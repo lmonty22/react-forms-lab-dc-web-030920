1c829e96e4108d8fad1306deaafadaee8aed0f3d
lmonty22 <lindsay.montgomery@olympian.org>
2020-05-13 14:01:12 -0400

    halfway through deliverables

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,26 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleLogin = (event) => {
+      event.preventDefault()
+      console.log(event)
+      
+    }
   render() {
     return (
       <form>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" onChange={this.handleChange} value={this.state.username}/>
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" onChange={this.handleChange} value={this.state.password} />
           </label>
         </div>
         <div>
-          <button type="submit">Log in</button>
+          <button type="submit" onSubit={this.handleLogin}>Log in</button>
         </div>
       </form>
     );
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,22 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
 
+  handleChange =(e) => {
+    this.setState({
+      message: e.target.value
+    })
+  }
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <div>{this.props.maxChars - this.state.message.length} characters left </div>
+        <input type="text" name="message" id="message" onChange={this.handleChange} value={this.state.message}/>
       </div>
     );
   }
